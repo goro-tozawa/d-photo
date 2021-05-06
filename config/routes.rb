af5b5924f3f72d22e8a092d92e10f9f1bdf903aa
goro-tozawa <ralugoro.0905@icloud.com>
2021-05-06 16:21:45 +0900

    マイページ土台作り完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index, :show]
+  resources :users
   resources :photos 
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
